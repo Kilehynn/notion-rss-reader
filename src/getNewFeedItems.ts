ad4351c433455c5088909669fc2ec2765750e045
Zoroark <utybodev@gmail.com>
2022-02-25 09:45:41 +0000

    Update readme and report HTTP errors

--- src/getNewFeedItems.ts
+++ src/getNewFeedItems.ts
@@ -1,11 +1,21 @@
 import Parser from 'rss-parser'
 import { timeDifference } from './helpers'
+import fetch from 'node-fetch'
 
 const parser = new Parser()
 
 export const getNewFeedItems = async (feedUrl: string) => {
   try {
-    const { items: newFeedItems } = await parser.parseURL(feedUrl)
+    const content = await fetch(feedUrl, { redirect: 'follow' })
+    if (content.status > 299)
+      throw new Error(
+        `Error fetching feed: ${content.status}, ${
+          content.statusText
+        }. Content: ${await content.text()}`
+      )
+    const { items: newFeedItems } = await parser.parseString(
+      await content.text()
+    )
 
     return newFeedItems.filter((feedItem) => {
       const { pubDate } = feedItem
